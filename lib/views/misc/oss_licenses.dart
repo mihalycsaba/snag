a476afd00266566f8f3d7737581545994ec0645b
mihalycsaba <mihalycsaba91@gmail.com>
2026-08-16 16:49:31 +0300

    0.0.18

--- lib/views/misc/oss_licenses.dart
+++ lib/views/misc/oss_licenses.dart
@@ -42252,12 +42252,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.''',
 );
 
-/// snag 0.0.18-pre.07
+/// snag 0.0.18
 const _snag = Package(
   name: 'snag',
   description: 'Client application for SteamGifts',
   authors: [],
-  version: '0.0.18-pre.07',
+  version: '0.0.18',
   spdxIdentifiers: ['GPL-3.0'],
   isMarkdown: false,
   isSdk: false,
